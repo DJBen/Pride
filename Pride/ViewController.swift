bf0973ba3fa0d20f206d082d3052fb28c88c10f7
Ben Lu <blu@box.com>
2015-06-27 00:56:18 -0700

    Initial Commit

---
+++ Pride/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pride
+//
+//  Created by Ben Lu on 6/27/15.
+//  Copyright © 2015 DJ.Ben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
